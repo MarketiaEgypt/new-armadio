dbc24838c57f0c75a8cde536176243099fb04a8e
MarketiaEgypt <admin@marketiaegy.com>
2022-04-14 11:51:01 +0200

    edit flag website

--- project/settings.py
+++ project/settings.py
@@ -109,18 +109,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ar'
 
 LANGUAGES = [
-    ('en', 'English'),
     ('ar', 'العربية'),
+    ('en', 'English'),
 ]
 
-MODELTRANSLATION_LANGUAGES = ('EN', 'AR')
+MODELTRANSLATION_LANGUAGES = ('AR', 'EN')
 
-
-
-LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
